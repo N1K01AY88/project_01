dd579aeab28524d5fc56a092ee5dc0cd8505a799
N1K01AY88 <nikolay7771988@mail.ru>
2023-04-24 16:05:30 +0300

    Homework

---
+++ Homework.py
@@ -0,0 +1,13 @@
+# Задача 1.1.
+
+# Есть строка с перечислением песен
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+ss = my_favorite_songs
+# print (len(ss))
+print (ss[:14])
+print (ss[-13:])
+print (ss[16:30])
+print (ss[-26:-15])
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
